f279b45e6ef625d675947a58f380950ed25d6739
Benjamin Barad <benjamin.barad@gmail.com>
2021-10-11 19:58:53 -0700

    upgraded prep scripts

--- mask_and_convert_ply.py
+++ mask_and_convert_ply.py
@@ -1,3 +1,7 @@
+import gc
+import glob
+import os
+
 import click
 import numpy as np
 from pycurv import pycurv_io as io
@@ -5,68 +9,95 @@ from scipy.ndimage.morphology import distance_transform_edt
 import mrcfile
 import vtk
 
+TOLERANCE_DISTANCE_NM=4.5 # Heuristic to get complete surfaces but avoid spurious junk
 
 @click.command()
-@click.argument('input_surface', type=str)
-@click.argument('input_mrc', type=str)
-@click.argument('output_base', type=str)
-@click.option('-l', '--label', type=int, default=1)
+@click.argument('input_surfaces', type=str, nargs=-1)
+@click.argument('input_mrc', type=str, nargs=1)
+# @click.argument('output_base', type=str, nargs=1)
+@click.option('-l', '--labels', type=list, default=[1])
 @click.option('-d', '--mask_tolerance_distance', type=int, default=3)
-def convert(input_surface, input_mrc, output_base, mask_tolerance_distance, label):
-    mrc = mrcfile.mmap(input_mrc, mode="r+", permissive=True)
-    header = mrc.header
-    maximum = (header.nx, header.ny, header.nz)
-    minimum = (0,0,0)
-    # print(maximum)
-    # print(minimum)
-    voxel_size = mrc.voxel_size.x/10 # nm
-    # print([round(i) for i in (maximum/voxel_size)])
-    plyfile = vtk.vtkPLYReader()
-    plyfile.SetFileName(input_surface)
-    plyfile.Update()
-    surf = plyfile.GetOutput() 
-    writer = vtk.vtkXMLPolyDataWriter()
-    fname = output_base + "_unmasked.vtp"
-    writer.SetFileName(fname)
-    writer.SetInputData(surf)
-    if writer.Write() != 1:
-        raise pexceptions.PySegInputError(
-            expr='save_vtp', msg='Error writing the file {}.'.format(fname))
+def convert_from_CLI(input_surfaces, input_mrc, mask_tolerance_distance, labels):
+    """Click wrapper for convert and mask script"""
+    assert(len(labels)==len(input_surfaces))
+    convert(input_surfaces, input_mrc, mask_tolerance_distance, labels)
 
-    mask = (mrc.data == label).astype(int)
-    dist_from_mask = distance_transform_edt(mask == 0)
-    for i in range(surf.GetNumberOfCells()):
-        # Check if all points which made up the polygon are in the mask
-        points_cell = surf.GetCell(i).GetPoints()
-        count = 0
-        for j in range(0, points_cell.GetNumberOfPoints()):
-            x,y,z = points_cell.GetPoint(j)
-            x = int(round(x/voxel_size))
-            y = int(round(y/voxel_size))
-            z = int(round(z/voxel_size))
-            # print(x,y,z)
-            if z>=maximum[2] or z<=minimum[2] or x>=maximum[0] or x<=minimum[0] or y>=maximum[1] or y<=minimum[1]:
-                count += 1
-                continue
-            if (dist_from_mask[
-                    z, y, x] >
-                    mask_tolerance_distance):
-                count += 1
-            # else: print(x,y,z)
-        # Mark cells that are not completely in the mask for deletion
-        if count > 0:
-            surf.DeleteCell(i)
+def convert(input_surfaces, input_mrc, mask_tolerance_distance, labels):
+    print("open")
+    with mrcfile.mmap(input_mrc, mode="r+", permissive=True) as mrc:
+        header = mrc.header
+        maximum = (header.nx, header.ny, header.nz)
+        minimum = (0,0,0)
+        # print(maximum)
+        # print(minimum)
+        voxel_size = mrc.voxel_size.x/10 # nm
+        # maximum = [int(i) for i in maximum]
+        print(maximum)
+        # Since I am keeping everything to voxel spacing until the scale step in pycurv, I will just reduce mask_tolerance_distance according to voxel size
+        mask_tolerance_distance = mask_tolerance_distance/voxel_size
+        print(voxel_size, mask_tolerance_distance)
+        # print([round(i) for i in (maximum/voxel_size)])
+        for index,input_surface in enumerate(input_surfaces):
+            label=labels[index]
+            print(label, input_surface)
 
-    # Delete
-    surf.RemoveDeletedCells()
-    
-    writer = vtk.vtkXMLPolyDataWriter()
-    fname = output_base + ".surface.vtp"
-    writer.SetFileName(fname)
-    writer.SetInputData(surf)
-    if writer.Write() != 1:
-        raise pexceptions.PySegInputError(
-            expr='save_vtp', msg='Error writing the file {}.'.format(fname))
+            output_base = input_surface[:-4]
+            plyfile = vtk.vtkPLYReader()
+            plyfile.SetFileName(input_surface)
+            plyfile.Update()
+            surf = plyfile.GetOutput() 
+            writer = vtk.vtkXMLPolyDataWriter()
+            fname = output_base + "_unmasked.vtp"
+            writer.SetFileName(fname)
+            writer.SetInputData(surf)
+            if writer.Write() != 1:
+                raise pexceptions.PySegInputError(
+                    expr='save_vtp', msg='Error writing the file {}.'.format(fname))
+            print("Making the mask")
+            mask = (mrc.data == label).astype(int)
+            # print(mask)
+            dist_from_mask = distance_transform_edt(mask == 0)
+            # print(dist_from_mask)
+            for i in range(surf.GetNumberOfCells()):
+                # Check if all points which made up the polygon are in the mask
+                points_cell = surf.GetCell(i).GetPoints()
+                # print(points_cell.GetPoint(0))
+                count = 0
+                for j in range(0, points_cell.GetNumberOfPoints()):
+                    x,y,z = [int(round(i)) for i in points_cell.GetPoint(j)]
+                    # print(x,y,z)
+                    # print(dist_from_mask[z, y, x])
+
+                    # x = int(round(x/voxel_size))
+                    # y = int(round(y/voxel_size))
+                    # z = int(round(z/voxel_size))
+                    # print(x,y,z)
+                    if z>=maximum[2] or z<=minimum[2] or x>=maximum[0] or x<=minimum[0] or y>=maximum[1] or y<=minimum[1]:
+                        count += 1
+                        continue
+                    if (dist_from_mask[z, y, x] >mask_tolerance_distance):
+                        count += 1
+                    # else: print(x,y,z)
+                # Mark cells that are not completely in the mask for deletion
+                if count > 0:
+                    surf.DeleteCell(i)
+
+            # Delete
+            surf.RemoveDeletedCells()
+            
+            writer = vtk.vtkXMLPolyDataWriter()
+            fname = output_base + ".surface.vtp"
+            writer.SetFileName(fname)
+            writer.SetInputData(surf)
+            if writer.Write() != 1:
+                raise pexceptions.PySegInputError(
+                    expr='save_vtp', msg='Error writing the file {}.'.format(fname))
+            del surf
+            del plyfile
+            del writer
+            del mask
+            del dist_from_mask
+            gc.collect()
 
 
 
@@ -74,4 +105,30 @@ def convert(input_surface, input_mrc, output_base, mask_tolerance_distance, labe
 
 
 if __name__=="__main__":
-    convert()
+    print("test")
+    # convert_from_CLI()
+    mrcfiles = glob.glob("*_labels.mrc")
+    # mrcfiles = ["UF6_labels.mrc"]
+    structures = ["OMM", "IMM", "ER"]
+    # structures=["IMM"]
+    print(mrcfiles)
+    for mrc in mrcfiles:
+        print(mrc)
+        basename = mrc[:-11]
+        print(basename)
+        surface_names = []
+        labels = []
+        for index,structure in enumerate(structures):
+            label = index+1
+            # print(label)
+            filename = basename+"_"+structure+".ply" 
+            if os.path.isfile(filename):
+                labels.append(label)
+                surface_names.append(filename)
+            else:
+                print(f"{filename} does not exist - skipping")
+        convert(surface_names, mrc, mask_tolerance_distance=TOLERANCE_DISTANCE_NM, labels=labels) # 4nm tolerance to close holes
+
+
+
+
--- mrc2pts.py
+++ mrc2pts.py
@@ -1,4 +1,5 @@
 import click
+import glob
 import mrcfile
 import numpy as np
 import pandas
@@ -7,11 +8,11 @@ import pandas
 @click.command()
 @click.argument('input', type=str)
 @click.argument('output', type=str)
-@click.option('-l', '--label', type=int, default=1)
+@click.option('-l', '--label', type=int, default=1, help="Label for feature of interest")
 def convert(input, output, label):
 	mrc = mrcfile.mmap(input, mode="r+", permissive=True)
-	voxel_size = mrc.voxel_size.x/10 # nm
-	# voxel_size = 1
+	# voxel_size = mrc.voxel_size.x/10 # nm
+	voxel_size = 1
 	print(voxel_size)
 	data = np.where(mrc.data == label)
 	print(data)
@@ -20,7 +21,25 @@ def convert(input, output, label):
 	df.to_csv(output, sep=" ", index=False, header=False)
 
 
-
+def convert_mitochondria(input_filename):
+	labels = ["OMM", "IMM", "ER"]
+	base = input_filename.split("_")[0]
+	mrc = mrcfile.mmap(input_filename, mode="r+", permissive=True)
+	# voxel_size = mrc.voxel_size.x/10 # nm
+	# voxel_size = 1
+	# print(voxel_size)
+	for index, label in enumerate(labels):
+		data = np.where(mrc.data == index+1)
+		# print(data)
+		df = pandas.DataFrame(data={'x': data[2], 'y': data[1], 'z': data[0]})
+		# df = df * voxel_size 
+		output = base+"_{}.pts".format(label)
+		print(output)
+		df.to_csv(output, sep=" ", index=False, header=False)
 
 if __name__=="__main__":
-	convert()
+	data = glob.glob("*_labels.mrc")
+	for datum in data:
+		print(datum)
+		convert_mitochondria(datum)
+	# convert()
