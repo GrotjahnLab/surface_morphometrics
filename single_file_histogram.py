109bc32fef5db93d228f4d581905d9b04d3093d8
Benjamin Barad <benjamin.barad@gmail.com>
2022-04-28 20:50:25 -0700

    stats library and test scripts working

---
+++ single_file_histogram.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+"""Generate an area weighted histogram of a single feature from a single mesh from a single segmentation
+Useful to get a sense of how your configuration for meshing and quantification looks. Also nice for responding to reviewer requests!
+
+Usage: python single_file_histogram.py input_csv.csv -n featurename
+"""
+
+__author__ = "Benjamin Barad"
+__email__ = "benjamin.barad@gmail.com"
+__license__ = "GPLv3"
+
+
+import os
+import click
+import pandas as pd
+from morphometrics_stats import histogram, weighted_median
+
+@click.command()
+@click.argument('input', type=str)
+@click.option('-n', '--featurename', type=str, required=True, default='curvedness_VV', help="Name of feature to extract")
+def main(input, featurename):
+    """Generate an area weighted histogram of a single feature from a single mesh from a single segmentation
+    Useful to get a sense of how your configuration for meshing and quantification looks. Also nice for responding to reviewer requests!
+    """
+    df = pd.read_csv(input)
+    data = df[featurename].values
+    areas = df['area'].values
+    output_name = input[:-4] + '_' + featurename + '.svg'
+    print('Generating histogram for feature {}'.format(featurename))
+    print('Area Weighted Median: {}'.format(weighted_median(data, areas)))
+    histogram([data], [areas], [os.path.basename(input)[:-4]], os.path.basename(input)+ " "+ featurename, featurename, filename=output_name)
+
+if __name__ == '__main__':
+    main()
