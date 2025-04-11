0bbdaad4c9085253941392210f1d28802a80c2b8
Benjamin Barad <benjamin.barad@gmail.com>
2025-04-11 09:37:49 -0700

    Typo

--- single_file_histogram.py
+++ single_file_histogram.py
@@ -18,7 +18,7 @@ from morphometrics_stats import histogram, weighted_median
 @click.command()
 @click.argument('input', type=str)
 @click.option('-n', '--featurename', type=str, required=True, default='curvedness_VV', help="Name of feature to extract")
-@click.option('-s','--figuresize', type=tuple, required=False, default=(11,8.5),help="Size of figure in inches (x,y))
+@click.option('-s','--figuresize', type=tuple, required=False, default=(11,8.5),help="Size of figure in inches (x,y)")
 def main(input, featurename, figuresize):
     """Generate an area weighted histogram of a single feature from a single mesh from a single segmentation
     Useful to get a sense of how your configuration for meshing and quantification looks. Also nice for responding to reviewer requests!
