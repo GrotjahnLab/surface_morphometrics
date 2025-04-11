1b96bbd8298b5aa17f46c62738b260260ac44481
Benjamin Barad <benjamin.barad@gmail.com>
2025-04-11 09:02:08 -0700

    Update single_file_histogram.py

--- single_file_histogram.py
+++ single_file_histogram.py
@@ -18,7 +18,8 @@ from morphometrics_stats import histogram, weighted_median
 @click.command()
 @click.argument('input', type=str)
 @click.option('-n', '--featurename', type=str, required=True, default='curvedness_VV', help="Name of feature to extract")
-def main(input, featurename):
+@click.option('-s','--figuresize', type=tuple, required=False, default=(11,8.5),help="Size of figure in inches (x,y))
+def main(input, featurename, figuresize):
     """Generate an area weighted histogram of a single feature from a single mesh from a single segmentation
     Useful to get a sense of how your configuration for meshing and quantification looks. Also nice for responding to reviewer requests!
     """
@@ -28,7 +29,7 @@ def main(input, featurename):
     output_name = input[:-4] + '_' + featurename + '.svg'
     print('Generating histogram for feature {}'.format(featurename))
     print('Area Weighted Median: {}'.format(weighted_median(data, areas)))
-    histogram([data], [areas], [os.path.basename(input)[:-4]], os.path.basename(input)+ " "+ featurename, featurename, filename=output_name)
+    histogram([data], [areas], [os.path.basename(input)[:-4]], os.path.basename(input)+ " "+ featurename, featurename, filename=output_name, figsize=figuresize)
 
 if __name__ == '__main__':
     main()
